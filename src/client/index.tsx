852b5de16468e65786fc63e7b1d0ad8a2a36aeab
Aaron Thompson <thompson_ad@outlook.com>
2020-04-19 10:36:50 +0100

    add first failing test

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,9 +1,7 @@
-import * as React from 'react';
-import * as ReactDOM from 'react-dom';
+// import * as React from 'react';
+// import * as ReactDOM from 'react-dom';
 
-import { Hello } from './Hello';
-
-ReactDOM.render(
-  <Hello compiler="TypeScript" framework="React" />,
-  document.getElementById('root'),
-);
+// ReactDOM.render(
+//   <Hello compiler="TypeScript" framework="React" />,
+//   document.getElementById('root'),
+// );
